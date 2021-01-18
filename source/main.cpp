f27bbfcc7cf4fb71d26366ef154d82bfdacf2570
italoucsp <42581127+italoucsp@users.noreply.github.com>
2021-01-18 00:35:05 -0500

    Possible representation

--- source/main.cpp
+++ source/main.cpp
@@ -1,42 +1,45 @@
 // Copyright 2020 Roger Peralta Aranibar Advanced Data Estructures
+#pragma warning ( disable : 4244)
+
 #include <iomanip>
 #include <iostream>
 #include <memory>
 #include <utility>
 #include <vector>
+#include <string>
+#include <chrono>
+#include <typeinfo>
+#include <functional>
 
-template <typename>
-class Timer;
+namespace timer_aed {
+#define MINUTES      std::chrono::minutes
+#define SECONDS      std::chrono::seconds
+#define MILLISECONDS std::chrono::milliseconds
+#define MICROSECONDS std::chrono::microseconds
+#define NANOSECONDS  std::chrono::nanoseconds
 
-template <typename R, typename... T>
-class Timer<R(T...)> {
- public:
-  typedef R (*function_type)(T...);
-  function_type function;
+  std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+  std::string uom_abreviation;
+  int64_t duration;
 
-  explicit Timer(function_type function, std::string process_name = "")
-      : function_(function), process_name_(process_name) {}
+#define get_var_name(x) #x
+#define TEST_FUNCTION(current_process) std::function<void()> current_process = []
 
-  R operator()(T... args) {
-    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-    start = std::chrono::high_resolution_clock::now();
-
-    R result = function_(std::forward<T>(args)...);
-
-    end = std::chrono::high_resolution_clock::now();
-    int64_t duration =
-        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
-            .count();
-
-    std::cout << std::setw(10) << process_name_ << std::setw(30)
-              << "Duration: " + std::to_string(duration) + " ns\n";
-    return result;
-  }
-
- private:
-  function_type function_;
-  std::string process_name_;
+#define RUN_TIMER(test_function_, unit_of_measurement)\
+        if(typeid(unit_of_measurement) == typeid(std::chrono::minutes)) uom_abreviation = "min.";\
+        else if(typeid(unit_of_measurement) == typeid(std::chrono::seconds)) uom_abreviation = "s.";\
+        else if(typeid(unit_of_measurement) == typeid(std::chrono::milliseconds)) uom_abreviation = "ms.";\
+        else if(typeid(unit_of_measurement) == typeid(std::chrono::microseconds)) uom_abreviation = "mcs.";\
+        else if(typeid(unit_of_measurement) == typeid(std::chrono::nanoseconds)) uom_abreviation = "ns.";\
+        start = std::chrono::high_resolution_clock::now();\
+        test_function_();\
+        end = std::chrono::high_resolution_clock::now();\
+        duration = std::chrono::duration_cast<unit_of_measurement>(end - start).count();\
+        std::cout << std::setw(10) << get_var_name(test_function_) << std::setw(30)\
+        << "Duration: " + std::to_string(duration) + " " + uom_abreviation + "\n"
 };
+
+using namespace timer_aed;
 
 class MyDataStructure {
  public:
@@ -58,24 +61,38 @@ class MyDataStructure {
 
 MyDataStructure G_DS;
 
-int build_data_structure() {
+void build_data_structure() {
   for (float i = 0; i < 10; ++i) {
     G_DS.add(i);
   }
-  return 0;
 }
 
-std::vector<std::vector<float>> query_knn(std::vector<float> query, int k) {
-  return G_DS.get_data(query, k);
+void query_knn() {
+  std::vector<float> query = { 1, 2, 3 };
+  int k = 2;
+  for (auto v : G_DS.get_data(query, k)) {
+    for (auto e : v)
+      std::cout << e << ' ';
+    std::cout << '\n';
+  }
 }
+
+std::vector<float> query;
 
 int main() {
-  Timer<int()> timed_built(build_data_structure, "Index");
-  timed_built();
+  TEST_FUNCTION(Indexation_test) {
+    for (float i = 0; i < 10; ++i) {
+      G_DS.add(i);
+    }
+  };
 
-  Timer<std::vector<std::vector<float>>(std::vector<float>, int)> timed_query(
-      query_knn, "Query KNN");
-  std::vector<float> query = {1, 2, 3};
-  int k = 2;
-  std::vector<std::vector<float>> result = timed_query(query, k);
+  query = { 1, 2, 3 };
+
+  TEST_FUNCTION(Query_KNN_test) {
+    G_DS.get_data(query, 2);
+  };
+
+  RUN_TIMER(Indexation_test, NANOSECONDS);
+  RUN_TIMER(Query_KNN_test, MICROSECONDS);
+  return 0;
 }
