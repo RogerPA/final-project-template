3644f7971f0750138e1242b195970aa237d62175
italoucsp <42581127+italoucsp@users.noreply.github.com>
2021-01-18 19:07:30 -0500

    unnecessary defines removed

--- source/main.cpp
+++ source/main.cpp
@@ -31,12 +31,6 @@
     #include <vector>
   #endif
 
-  #define NC "\e[0m"
-  #define RED "\e[0;31m"
-  #define GRN "\e[0;32m"
-  #define CYN "\e[0;36m"
-  #define REDB "\e[41m"
-
   typedef std::chrono::minutes      MINUTES;
   typedef std::chrono::seconds      SECONDS;
   typedef std::chrono::milliseconds MILLISECONDS;
